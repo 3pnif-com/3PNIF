edd780e48ba2a4d4369327c79fa75b47dacd4cd4
3PNIF <51055618+3pnif-com@users.noreply.github.com>
2019-06-07 23:48:00 +0100

    Add files via upload

--- about.php
+++ about.php
@@ -21,8 +21,7 @@
     			3PNIF helps to carry out a risk assessment of information and systems security by performing tests containing the selected tools for information and identification of vulnerabilities or security breaches. This risk assessment presents results or information through the output of the tools, which allow to identify if they are in agreement with those expected or planned previously.
     			<br><br><strong>How did this work come about?</strong><br>
     			This project work arises in the scope of the 1st Masters Course in Cyber-Security and Forensic Informatics (MCIF) of the Higher School of Technology and Management (ESTG) of the Polytechnic Institute of Leiria (IPL) in the 2017/2018 academic year and its presentation and public discussion aim at obtaining the Master's degree in this scientific area.
-    			<br><br>Visit www.3pnif.com for more information and access to the latest versions and news.<br>
-    			Any question or suggestion, please send email to info@3pnif.com<br>
+    			<br><br>Visit <a href="https://github.com/3pnif-com/3pnif" target="_blank">https://github.com/3pnif-com/3pnif</a> for more information and access to the latest versions and news.<br>
     			<br>
     			Cristiano P. Alves
     			<br>
